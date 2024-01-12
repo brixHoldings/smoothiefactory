84cace7a6371a8c688fdad3abd438f33ad3f4dbd
Rudi Aj <raj@dickeys.com>
2024-01-12 11:44:27 +0100

    fix: update packages

--- prismicio.ts
+++ prismicio.ts
@@ -48,11 +48,11 @@ const routes: prismic.ClientConfig['routes'] = [
  */
 export const createClient = (config: prismicNext.CreateClientConfig = {}) => {
   const client = prismic.createClient(repositoryName, {
+    fetchOptions: {
+      cache: process.env.NODE_ENV === 'production' ? 'force-cache' : 'no-store',
+      next: { tags: ['prismic'] },
+    },
     routes,
-    fetchOptions:
-      process.env.NODE_ENV === 'production'
-        ? { next: { tags: ['prismic'] }, cache: 'force-cache' }
-        : { next: { revalidate: 5 } },
     ...config,
   });
 
