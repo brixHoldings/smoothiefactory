cb5ab6de89929511433fed15d0c71d6e741b6092
Rudi Aj <raj@dickeys.com>
2024-02-27 15:08:21 +0100

    fix: add gtm script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,14 @@ const nexa = localFont({
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html className={nexa.variable} lang="en">
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WG42BBT');
+`}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-7217011BH2"
         id="google-tag-manager"
@@ -53,6 +61,14 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
         `}
       </Script>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WG42BBT"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <StoreProvider>
           <StyledComponentsRegistry>
             <GlobalStyle />
