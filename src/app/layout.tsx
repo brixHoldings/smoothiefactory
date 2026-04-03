7e5cc60893b6704427fabdd76708dac839dcfe7d
JohnHoward-Brix <157166380+JohnHoward-Brix@users.noreply.github.com>
2026-04-03 10:17:01 -0500

    Update layout.tsx with cookieyes script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
 'use client';
-
 import localFont from 'next/font/local';
 import Script from 'next/script';
 import { useSelectedLayoutSegments } from 'next/navigation';
 import { PrismicPreview } from '@prismicio/next';
-
 import Footer from '@components/pages/SmoothieFactory/OrangeLeaf/common/Footer';
 import Header from '@components/pages/SmoothieFactory/OrangeLeaf/common/Header';
 import StyledComponentsRegistry from '@styles/registry';
-
 import type { FC, ReactNode } from 'react';
 import GlobalStyle from './GlobalStyle';
 import { StoreProvider } from 'src/context/StoreProvider';
@@ -38,27 +35,22 @@ const nexa = localFont({
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <html className={nexa.variable} lang="en">
-      <Script>
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WG42BBT');
-`}
-      </Script>
+      {/* CookieYes - must load first before analytics scripts */}
+      <Script
+        id="cookieyes"
+        src="https://cdn-cookieyes.com/client_data/94bd2bc38ad1681086727be5106b55ae/script.js"
+        strategy="beforeInteractive"
+      />
 
+      {/* Google Tag Manager */}
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WG42BBT');`}
+      </Script>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-7217011BH2"
         id="google-tag-manager"
         strategy="afterInteractive"
-      >
-        {`
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-7217011BH2');
-        `}
-      </Script>
+      />
       <body>
         <noscript>
           <iframe
@@ -77,27 +69,27 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <Footer />
           </StyledComponentsRegistry>
         </StoreProvider>
+        <Script
+          id="acsb-script"
+          strategy="lazyOnload"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                var s = document.createElement('script');
+                var h = document.querySelector('head') || document.body;
+                s.src = 'https://acsbapp.com/apps/app/dist/js/app.js';
+                s.async = true;
+                s.onload = function() {
+                  if (typeof acsbJS !== 'undefined') {
+                    acsbJS.init();
+                  }
+                };
+                h.appendChild(s);
+              })();
+            `,
+          }}
+        />
       </body>
-      <Script
-        id="acsb-script"
-        strategy="lazyOnload" // Loads after everything else
-        dangerouslySetInnerHTML={{
-          __html: `
-        (function() {
-          var s = document.createElement('script');
-          var h = document.querySelector('head') || document.body;
-          s.src = 'https://acsbapp.com/apps/app/dist/js/app.js';
-          s.async = true;
-          s.onload = function() {
-            if (typeof acsbJS !== 'undefined') {
-              acsbJS.init();
-            }
-          };
-          h.appendChild(s);
-        })();
-      `,
-        }}
-      />
     </html>
   );
 };
